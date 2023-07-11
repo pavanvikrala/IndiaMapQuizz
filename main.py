6e716ea80342ed130c5b7420914e67989f37c921
Vikrala Pavan Kumar <pavanvikrala@gmail.com>
2023-07-11 17:53:16 +0530

    main.py

--- main.py
+++ main.py
@@ -22,11 +22,12 @@ while len(guessed_states) < 36:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/36 States/UT Correct",
                                     prompt="What's another state/UT's name?").title()
     if answer_state == 'Exit':
+        # If the user types in exit, the quiz will end and the program creates a new csv file which contains all the unanswered states 
         missing_states = [state for state in states if state not in guessed_states]
         new_data = pd.DataFrame(missing_states)
         new_data.to_csv("missing_locations.csv")
         break
-
+    
     if answer_state in states:
         guessed_states.append(answer_state)
         new_turtle = Turtle()
